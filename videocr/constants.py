84d8f210a9fefe1fde9c38b335db204e36e8b26f
mzn928 <16765128+mzn928@users.noreply.github.com>
2023-07-14 09:47:32 +0330

    Update constants.py

--- videocr/constants.py
+++ videocr/constants.py
@@ -1,6 +1,6 @@
 import pathlib
 
-TESSDATA_DIR = PureWindowsPath('c:/Program Files/Tesseract-OCR') / 'tessdata' # Tesseract path for windwos
+TESSDATA_DIR = Path('c:/Program Files/Tesseract-OCR/tessdata')  # Tesseract path for windwos
 
 TESSDATA_URL = 'https://github.com/tesseract-ocr/tessdata_fast/raw/main/{}.traineddata'
 
